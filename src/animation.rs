6a6a1458517c90cda0dedc661bd69dfee7373317
Serika Yuzuki <ReckySet@outlook.jp>
2023-08-16 02:15:27 +0900

    learnt atlas

--- src/animation.rs
+++ src/animation.rs
@@ -1,174 +1,172 @@
-use bevy::{prelude::*, ecs::query};
+use bevy::{prelude::*, ecs::query, asset::LoadState};
 use crate::{Player, PlayerState};
 
 
 /////////////////////////////////////////////
 /// Setting up the plugin
 /////////////////////////////////////////////
-pub struct HelloPlugin;
+pub struct PlayerAnimationPlugin;
 
-impl Plugin for HelloPlugin {
+impl Plugin for PlayerAnimationPlugin {
     fn build(&self, app: &mut App) {
-        // app.add_systems(Startup, load_idle_image);
-        // app.add_systems(Update,animate_sprite);
-
+        app
+        .init_resource::<PlayerSpriteHandles>()
+        .add_state::<AppState>()
+        .add_systems(OnEnter(AppState::Setup), load_images)
+        .add_systems(Update, check_if_image_loaded.run_if(in_state(AppState::Setup)))
+        .add_systems(OnEnter(AppState::Finished), setup)
+        .add_systems(Update, animate_sprite);
     }
 }
 
-/////////////////////////////////////////////
-/// Component Section
-/////////////////////////////////////////////
-/// 
-/// This component is container for animation counter 
+/// Component : Player Sprite Handles
+/// Contains Vec<UntypedHandle>
+#[derive(Resource, Default)]
+struct PlayerSpriteHandles {
+    handles: Vec<HandleUntyped>,
+}
+
 #[derive(Component)]
-pub struct AnimationIndices {
+struct PlayerTextureAtlasHandles {
+    idle_texture_atlas_handle: Handle<TextureAtlas>,
+    jump_texture_atlas_handle: Handle<TextureAtlas>,
+}
+
+/// Component : Player Sprite Index
+/// Contains Number of Sprites and Indexes for each animation
+#[derive(Component)]
+struct AnimationIndices {
     first: usize,
     last: usize,
 }
 
+/// Component : Player Sprite Index Container
+/// Contains AnimationIndices for each animation
+#[derive(Component)]
+struct AnimationIndicesContainer {
+    idle: AnimationIndices,
+    jump: AnimationIndices,
+}
 
-/// Animation Timer Component
+/// Component : Player Sprite Animation Timer
 #[derive(Component, Deref, DerefMut)]
-pub struct AnimationTimer(Timer);
+struct AnimationTimer(Timer);
 
-pub fn animate_sprite(
+/// Component : AppState for Loading
+/// Contains LoadState : Loading, Failed, Loaded
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash, States)]
+enum AppState {
+    #[default]
+    Setup,
+    Finished,
+}
+
+/// System : Load Player Sprites Images
+fn load_images (
+    mut player_sprite_handles: ResMut<PlayerSpriteHandles>,
+    asset_server: Res<AssetServer>,
+){
+    player_sprite_handles.handles = asset_server.load_folder("textures").unwrap();
+}
+
+/// System : Check if Player Sprites Images are loaded
+fn check_if_image_loaded(
+    asset_server: Res<AssetServer>,
+    player_sprite_handles: ResMut<PlayerSpriteHandles>,
+    mut state: ResMut<NextState<AppState>>,
+){
+    if let 
+    LoadState::Loaded = asset_server.get_group_load_state(
+        player_sprite_handles.handles.iter().map(|h| h.id())) 
+        {
+        state.set(AppState::Finished);
+    }
+}
+
+
+/// System : Add Sprite Sheet Components to Player
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+    player_sprite_handles: ResMut<PlayerSpriteHandles>,
+    mut textures: ResMut<Assets<Image>>,
+){
+
+    // Build Texure Atlas
+    let mut texture_atlas_builder = TextureAtlasBuilder::default();
+
+    // Check All Images Loaded and Add to Texture Atlas
+    for handle in &player_sprite_handles.handles {
+        let handle = handle.typed_weak();
+        let Some(texture) = textures.get(&handle) else {
+            warn!("{:?} did not resolve to an `Image` asset.", asset_server.get_handle_path(handle));
+            continue;
+        };
+
+        texture_atlas_builder.add_texture(handle, texture);
+    }
+
+    let texture_atlas = texture_atlas_builder.finish(&mut textures).unwrap();
+    texture_atlases.add(texture_atlas);
+
+    let idle_sheet_image_handle : Handle<Image> = player_sprite_handles.handles[0].clone().typed();
+
+    let idle_texture_atlas =
+        TextureAtlas::from_grid(idle_sheet_image_handle, Vec2::new(80.0, 96.0), 10, 5, None, None);
+
+    let player_idle_animation_indices = AnimationIndices {
+        first: 0,
+        last: 11,
+    };
+    let player_jump_animation_indices = AnimationIndices {
+        first: 26,
+        last: 42,
+    };
+    let player_animation_indices_container = AnimationIndicesContainer {
+        idle: player_idle_animation_indices,
+        jump: player_jump_animation_indices,
+    };
+
+
+    let texture_atlas_handle: Handle<TextureAtlas> = texture_atlases.add(idle_texture_atlas);
+
+    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn((
+        SpriteSheetBundle {
+            texture_atlas: texture_atlas_handle,
+            sprite: TextureAtlasSprite::new(player_animation_indices_container.idle.first),
+            transform: Transform::from_scale(Vec3::splat(6.0)),
+            ..default()
+        },
+        player_animation_indices_container,
+        AnimationTimer(Timer::from_seconds(0.07, TimerMode::Repeating)),
+    ));
+}
+
+// System : Update Animate Player Sprites
+fn animate_sprite(
     time: Res<Time>,
+    keyboard_input: Res<Input<KeyCode>>,
     mut query: Query<(
-        &AnimationIndices,
+        &AnimationIndicesContainer,
         &mut AnimationTimer,
         &mut TextureAtlasSprite,
-        
     )>,
-    mut query2: Query<(&Player,
-        &PlayerState)>,
 ) {
-
-    if query2.iter().len() == 0 {
-        println!("No Player");
-        return;
-    } else {
-        let (player, player_state) = &mut query2.get_single().unwrap();
-        match player_state {
-            PlayerState::Idle => {
-                println!("Idle");
-                for (indices, mut timer, mut sprite) in &mut query {
-
-                    timer.tick(time.delta());
-                    if timer.just_finished() {
-                        sprite.index = if sprite.index == indices.last {
-                            indices.first
-                        } else {
-                            sprite.index + 1
-                        };
-                    }
-            
-                }
-            },
-            PlayerState::Run => {
-                println!("Run");
-                for (indices, mut timer, mut sprite) in &mut query {
-
-                    timer.tick(time.delta());
-                    if timer.just_finished() {
-                        sprite.index = if sprite.index == indices.last {
-                            indices.first
-                        } else {
-                            sprite.index + 1
-                        };
-                    }
-            
-                }
-            },
+    if keyboard_input.pressed(KeyCode::A) {
+        for (indices, mut timer, mut sprite) in &mut query {
+            timer.tick(time.delta());
+            if timer.just_finished() {
+                sprite.index = if sprite.index == indices.idle.last {
+                    indices.idle.first
+                } else {
+                    sprite.index + 1
+                };
+            }
         }
     }
-    // TODO Switch animation based on player or enemy state
     
 }
 
-fn animate (time: Res<Time>,
-    mut query: Query<(
-        &AnimationIndices,
-        &mut AnimationTimer,
-        &mut TextureAtlasSprite,
-        
-    )>,){
-    for (indices, mut timer, mut sprite) in &mut query {
-
-        timer.tick(time.delta());
-        if timer.just_finished() {
-            sprite.index = if sprite.index == indices.last {
-                indices.first
-            } else {
-                sprite.index + 1
-            };
-        }
-
-    }
-}
-
-#[derive(Component)]
-enum AnimationState {
-    Idle,
-    Run,
-}
-
-#[derive(Bundle)]
-pub struct PlayerAnimation{
-    spritesheetbundle: SpriteSheetBundle,
-    animation_indices: AnimationIndices,
-    animation_timer: AnimationTimer,
-    animation_state: AnimationState,
-}
-
-
-/// This component is List of animation counter
-#[derive(Resource, Default)]
-pub struct PlayerSpriteHandles {
-    idle: HandleUntyped,
-    run: HandleUntyped,
-}
-
-#[derive(Resource)]
-struct IdleSpriteSheet {
-    texture_atlas: Handle<TextureAtlas>,
-}
-
-pub fn load_idle_image(
-    asset_server: Res<AssetServer>,
-    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
-) -> PlayerAnimation
-{
-
-    // TODO Load Different animation indices from files
-    let texture_handle = asset_server.load("tewi_material01.png");
-    let texture_atlas =
-        TextureAtlas::from_grid(texture_handle, Vec2::new(80.0, 96.0), 10, 5, None, None);
-
-    let texture_atlas_handle = texture_atlases.add(texture_atlas);
-
-
-
-    let texture_handle = asset_server.load("tewi_material02.png");
-    let texture_atlas =
-    TextureAtlas::from_grid(texture_handle, Vec2::new(80.0, 96.0), 10, 5, None, None);
-
-    let texture_atlas_handle = texture_atlases.add(texture_atlas);
-
-
-
-    // Use only the subset of sprites in the sheet that make up the run animation
-    let animation_indices = AnimationIndices { first: 0, last: 11 };
-    // TODO Set animation into different component rand set it into player entity
-    let player_animation = PlayerAnimation{
-        spritesheetbundle: SpriteSheetBundle {
-        texture_atlas: texture_atlas_handle,
-        sprite: TextureAtlasSprite::new(animation_indices.first),
-        transform: Transform::from_scale(Vec3::splat(6.0)),
-        ..default()
-    },
-    animation_indices: animation_indices,
-    animation_timer: AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
-    animation_state: AnimationState::Idle,
-    };
-    player_animation
-}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 //! Renders a 2D scene containing a single, moving sprite.
 
-use animation::{HelloPlugin, AnimationIndices, AnimationTimer};
+use animation::PlayerAnimationPlugin;
 use bevy::{prelude::*, ecs::query};
 
 pub mod animation;
@@ -9,8 +9,7 @@ fn main() {
     let mut app = App::new();
     app.add_plugins(DefaultPlugins)
         .add_systems(Startup, add_player)
-        .add_plugins(HelloPlugin)
-        .add_systems(Update, play_player_animation)
+        .add_plugins(PlayerAnimationPlugin)
         .run();
 }
 
@@ -46,29 +45,5 @@ fn add_player(
     asset_server: Res<AssetServer>,
     texture_atlases: ResMut<Assets<TextureAtlas>>,
 ) {
-    let entity_id = 
-        commands.spawn(
-            (
-                Name("Player".to_string()), 
-                Position{x: 0.0, y: 0.0}, Velocity{dx: 0.0, dy: 0.0}, 
-                Player,
-                animation::load_idle_image(asset_server, texture_atlases),
-                PlayerState::Idle,
-            ),   
-        ).id();
-
-    commands.spawn(Camera2dBundle::default());
-}
-
-fn play_player_animation(
-    time: Res<Time>,
-    mut query: Query<(
-        &AnimationIndices,
-        &mut AnimationTimer,
-        &mut TextureAtlasSprite,
-    )>,
-    mut query2: Query<(&Player,
-        &PlayerState)>,
-){
-    animation::animate_sprite(time, query, query2);
+    
 }
